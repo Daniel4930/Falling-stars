424bb3aeb6335c86e97cc43351c210bef25f816a
Daniel <phule4108@gmail.com>
2023-02-19 13:46:43 -0800

    Clean up code and added sprites

--- main.py
+++ main.py
@@ -3,144 +3,126 @@ pygame.init()
 pygame.display.init()
 
 # Constant variables
-BROWN = (100,40,0)
 RED = (255,0,0)
 BACKGROUND_COLOR = (186,237,255)
+WINDOW_WIDTH, WINDOW_HEIGHT = 1000, 700
+STAR_SIZE, SLIME_SIZE, BORDER_THICKNESS = 60, 100, 30
+BORDER_LEFT_X, BORDER_LEFT_Y = 100, 0
+BORDER_RIGHT_X, BORDER_RIGHT_Y = 870, 0
+SCORE_BOARD_WIDTH, SCORE_BOARD_HEIGHT = 80, 50
 
-class Map():
-    def __init__(self):
-        pygame.font.init()
-        self.FONT = pygame.font.SysFont('Arial',25)
-        self.GAME_TITILE = pygame.display.set_caption("Falling Stars")
-        self.WINDOW_WIDTH, self.WINDOW_HEIGHT = 1000, 700
-        self.WINDOW = pygame.display.set_mode((self.WINDOW_WIDTH,self.WINDOW_HEIGHT))
+WINDOW = pygame.display.set_mode((WINDOW_WIDTH,WINDOW_HEIGHT))
+FONT = pygame.font.SysFont('Arial',25)
+GAME_TITILE = pygame.display.set_caption("Falling Stars")
 
-        self.BORDER_THICKNESS = 30
-        self.BORDER_LEFT_X, self.BORDER_LEFT_Y = 100, 0
-        self.BORDER_RIGHT_X, self.BORDER_RIGHT_Y = 870, 0
-        self.BORDER_IMAGE = pygame.image.load(os.path.join("wall_border.png")).convert_alpha()
-
-        self.SCORE_BOARD_WIDTH, self.SCORE_BOARD_HEIGHT = 80, 50
-        self.SCORE_BOARD = pygame.Rect(self.BORDER_RIGHT_X + self.BORDER_THICKNESS, 0, self.SCORE_BOARD_WIDTH, self.SCORE_BOARD_HEIGHT)
-        self.points = 0
-        self.font_surface = self.FONT.render("Score: " + str(self.points),False,RED)
-
-    def score(self,score):
-        self.points = score
-
-    def draw(self):
-        self.WINDOW.fill(BACKGROUND_COLOR)
-        self.font_surface = self.FONT.render("Score: " + str(self.points),False,RED)
-        self.score_board_image = pygame.draw.rect(self.WINDOW,BACKGROUND_COLOR,self.SCORE_BOARD)
-        self.WINDOW.blits([(self.BORDER_IMAGE,(self.BORDER_LEFT_X,self.BORDER_LEFT_Y)), (self.BORDER_IMAGE,(self.BORDER_RIGHT_X,self.BORDER_RIGHT_Y)),(self.font_surface,(self.BORDER_RIGHT_X+self.BORDER_THICKNESS,0))])
-
-class Slime(Map):
-    def __init__(self):
+class Slime(pygame.sprite.Sprite):
+    def __init__(self, x, y, left_move):
         super().__init__()
-        self.SLIME_SIZE = 100
-        self.x = (self.WINDOW_WIDTH/2) - (self.SLIME_SIZE/2)
-        self.y = self.WINDOW_HEIGHT-self.SLIME_SIZE
-        self.left_move = True
-        self.SLIME_RIGHT = pygame.transform.scale(pygame.image.load(os.path.join("character_right_image.png")).convert_alpha(),(self.SLIME_SIZE,self.SLIME_SIZE))
-        self.SLIME_LEFT = pygame.transform.scale(pygame.image.load(os.path.join("character_left_image.png")).convert_alpha(),(self.SLIME_SIZE,self.SLIME_SIZE))
+        self.x = x
+        self.y = y
+        self.left_move = left_move
+        if self.left_move == True:
+            self.image = pygame.transform.scale(pygame.image.load(os.path.join("character_left_image.png")).convert_alpha(),(SLIME_SIZE, SLIME_SIZE))
+        self.image = pygame.transform.scale(pygame.image.load(os.path.join("character_right_image.png")).convert_alpha(),(SLIME_SIZE, SLIME_SIZE))
+        self.rect = self.image.get_rect()
+        self.rect.topleft = [self.x, self.y]
 
-    def draw(self):
+    # Check for collision between a star and slime, only the star got deleted from the screen when collided
+    def collision(self, slime, star_group):
+        if pygame.sprite.collide_rect_ratio(0.75):
+            collided = pygame.sprite.spritecollide(slime, star_group, True)
+            return collided
+
+    def update(self, slime_movement_speed):
+        # Control the slime's movement using keyboard
+        key_pressed = pygame.key.get_pressed()
+        if key_pressed[pygame.K_LEFT]: # move left
+            self.x -= slime_movement_speed
+            self.rect.topleft = [self.x, self.y]
+            self.left_move = True
+        if key_pressed[pygame.K_RIGHT]: # move right
+            self.x += slime_movement_speed
+            self.rect.topleft = [self.x, self.y]
+            self.left_move = False
+
         if self.left_move == False:
-            self.WINDOW.blit(self.SLIME_RIGHT,(self.x,self.y))
+            # When slime moved right, display slime's image where it facing to the right
+            self.image = pygame.transform.scale(pygame.image.load(os.path.join("character_right_image.png")).convert_alpha(),(SLIME_SIZE, SLIME_SIZE))
         else:
-            self.WINDOW.blit(self.SLIME_LEFT,(self.x,self.y))
+            # When slime moved left, display slime's image where it facing to the left
+            self.image = pygame.transform.scale(pygame.image.load(os.path.join("character_left_image.png")).convert_alpha(),(SLIME_SIZE, SLIME_SIZE))
 
-class Star(Map):
-    def __init__(self):
+class Star(pygame.sprite.Sprite):
+    def __init__(self, x, y):
         super().__init__()
         self.STAR_SIZE = 60
-        self.x = random.randint(self.BORDER_LEFT_X + self.BORDER_THICKNESS,self.BORDER_RIGHT_X - self.STAR_SIZE)
-        self.y = -(self.STAR_SIZE)
-        self.STAR = pygame.transform.scale(pygame.image.load(os.path.join("star_image.png")).convert_alpha(),(self.STAR_SIZE,self.STAR_SIZE))
+        self.x, self.y = x, y
+        self.image = pygame.transform.scale(pygame.image.load(os.path.join("star_image.png")).convert_alpha(),(self.STAR_SIZE,self.STAR_SIZE))
+        self.rect = self.image.get_rect()
+        self.rect.topleft = [self.x, self.y]
 
-    def movement(self,star_movement_speed):
-        self.y += star_movement_speed
+    # Allow the stars to fall down from the top of the screen
+    def update(self, star_falling_speed):
+        self.y += star_falling_speed
+        self.rect.topleft = [self.x, self.y]
 
-    def create_star(self):
-        return Star()
+def draw(point):
+    BORDER_IMAGE = pygame.image.load(os.path.join("wall_border.png")).convert_alpha()
+    font_surface = FONT.render("Score: " + str(point), False, RED)
 
-    def draw(self):
-        self.WINDOW.blit(self.STAR,(self.x,self.y))
+    # Setup window's background, borders, and score board.
+    WINDOW.fill(BACKGROUND_COLOR)
+    WINDOW.blits([(BORDER_IMAGE, (BORDER_LEFT_X, BORDER_LEFT_Y)), (BORDER_IMAGE, (BORDER_RIGHT_X, BORDER_RIGHT_Y)),(font_surface, (BORDER_RIGHT_X + BORDER_THICKNESS, 0))])
 
 def main():
     now = pygame.time.get_ticks()
     frame = pygame.time.Clock()
     fps = 60
     score = 0
-    run = True
+    left_move, run = True, True
     slime_movement_speed = 5
     star_movement_speed = 2
 
-    map = Map()
-    slime = Slime()
-    star = Star()
-    star_list_object = []
+    slime = Slime((WINDOW_WIDTH/2) - (SLIME_SIZE/2), (WINDOW_HEIGHT - SLIME_SIZE), left_move)
+    slime_group = pygame.sprite.Group()
+    slime_group.add(slime)
+    star_group = pygame.sprite.Group()
         
     while run:
         frame.tick(fps)
         for event in pygame.event.get():
+            # To stop the game, click the red button on the window
             if event.type == pygame.QUIT:
                 run = False
 
-        # Give player control over the character on screen using keyboard's inputs.
-        key_pressed = pygame.key.get_pressed()
-        if key_pressed[pygame.K_LEFT]:
-            slime.x -= slime_movement_speed
-            slime.left_move = True
-        if key_pressed[pygame.K_RIGHT]:
-            slime.x += slime_movement_speed
-            slime.left_move = False
-        if key_pressed[pygame.K_SPACE]:
-            run = False
-
-        # This doesn't allow the player to go over the borders and the map
-        if slime.x < map.BORDER_LEFT_X + map.BORDER_THICKNESS + slime_movement_speed:
-            slime.x = map.BORDER_LEFT_X + map.BORDER_THICKNESS
-        if slime.x > map.BORDER_RIGHT_X - slime.SLIME_SIZE - slime_movement_speed:
-            slime.x = map.BORDER_RIGHT_X - slime.SLIME_SIZE
-        if slime.y > map.WINDOW_HEIGHT - slime.SLIME_SIZE:
-            slime.y = map.WINDOW_HEIGHT-slime.SLIME_SIZE
+        # This doesn't allow the slime to go over the borders and the map
+        if slime.x < BORDER_LEFT_X + BORDER_THICKNESS + slime_movement_speed:
+            slime.x = BORDER_LEFT_X + BORDER_THICKNESS + slime_movement_speed
+        if slime.x > BORDER_RIGHT_X - SLIME_SIZE - slime_movement_speed:
+            slime.x = BORDER_RIGHT_X - SLIME_SIZE - slime_movement_speed
+        if slime.y > WINDOW_HEIGHT - SLIME_SIZE:
+            slime.y = WINDOW_HEIGHT - SLIME_SIZE
         if slime.y < 0:
             slime.y = 0
 
-
-        # Allow the slime to eat stars, if the slime ate the star, delete that star from the screen
-        if len(star_list_object) > 0:
-            for i, star_object in enumerate(star_list_object):
-                if (slime.x <= star_object.x + (star.STAR_SIZE/2) <= slime.x + slime.SLIME_SIZE) and (slime.y <= (star_object.y + star.STAR_SIZE/2) <= slime.y + slime.SLIME_SIZE):
-                    score = score + 1
-                    map.score(score)
-                    delete_star = star_list_object.pop(i)
-                    del delete_star # Delete Star object
-        
-        # Delete Star's objects that passed the border
-        if len(star_list_object) == 3:
-            for i, star_object in enumerate(star_list_object):
-                if star_object.y > map.WINDOW_HEIGHT:   # If the star didn't get eaten by the slime and it's passed through the map, delete the star object
-                    delete_star = star_list_object.pop(i)
-                    del delete_star
-
-        # Create star objects
+        # Create star sprites and store all sprites a group
         cooldown = pygame.time.get_ticks() - now
-        if cooldown >= 4000:
-            star_object = star.create_star()    #Call the create_star() method from the Star class
-            star_list_object.append(star_object)
+        if cooldown >= 3000:
+            new_star = Star(random.randint(BORDER_LEFT_X + BORDER_THICKNESS, BORDER_RIGHT_X - STAR_SIZE), -(STAR_SIZE))
+            star_group.add(new_star)
             now = pygame.time.get_ticks()
 
-        # Make the stars falling down
-        for i in range(len(star_list_object)):
-            star_list_object[i].movement(star_movement_speed)
-
+        # If collided, increase the score by 1
+        collided_stars = slime.collision(slime, star_group)
+        for num_star in collided_stars:
+            score += 1
+        
         # Display the game
-        map.draw()
-        slime.draw()
-        for i in range(len(star_list_object)):
-            star_list_object[i].draw()
+        draw(score)
+        slime_group.draw(WINDOW)
+        slime_group.update(slime_movement_speed)
+        star_group.draw(WINDOW)
+        star_group.update(star_movement_speed)
         pygame.display.update()
 
 if __name__ == "__main__":
